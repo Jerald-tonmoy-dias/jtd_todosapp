6a5efce905ea9401d7d15093d4a96c2ac76258c5
Jerald Tonmoy Dias <tonmoyjareld@gmail.com>
2024-05-23 01:27:21 +0600

    add new todo to list

---
+++ android/app/src/main/kotlin/com/example/jtd_todosapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.jtd_todosapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:jtd_todosapp/util/dialog_box.dart';
+import 'package:jtd_todosapp/util/todo_tile.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+// text controller
+  final _controller = TextEditingController();
+
+// list of todo task
+
+  List toDoList = [
+    ['Make tutorial', false],
+    ['Learn flutter', false],
+    ['Earn money', false],
+  ];
+
+// handle checkbox function
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+// save New Task
+  void saveNewTask() {
+    setState(() {
+      toDoList.add([_controller.text, false]);
+      _controller.clear();
+    });
+    Navigator.of(context).pop();
+  }
+
+// createNewTask
+  void createNewTask() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return DialogBox(
+          controller: _controller,
+          onSave: saveNewTask,
+          onCancel: () => Navigator.of(context).pop(),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        child: Icon(Icons.add),
+      ),
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            "JTD TODO",
+          ),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.yellow,
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return TodoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+          );
+        },
+      ),
+    );
+  }
+}
